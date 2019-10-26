fbd9ecb1bea21faedb364cd363af4a8844aa594b
Andrew G <booleanorama@gmail.com>
2019-10-25 20:47:04 -0400

    Finish first POST request

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access public
@@ -15,9 +17,17 @@ exports.getBootcamp = (req, res, next) => {
 // @desc create new bootcamp
 // @route POST /api/v1/bootcamps/:id
 // @access private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'create new bootcamp' });
-};
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  });
+  } catch (err) {
+      res.status(400).json({ success: false });
+  }
+   };
 
 // @desc update bootcamp
 // @route PUT /api/v1/bootcamps/:id
