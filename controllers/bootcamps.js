651108df407652359ffcf1c44ff8c40e97b5a6de
Andrew G <booleanorama@gmail.com>
2019-10-28 07:42:29 -0400

    Add Geocoder

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,78 +1,68 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async')
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access public
-exports.getBootcamps = async (req, res, next) => {
-  try {
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+
     const bootcamps = await Bootcamp.find();
-    res.status(200).json({ success: true,
-      count: bootcamps.length,  data: bootcamps });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
+  }); 
+  
 
 // @desc get single botcamp
 // @route GET /api/v1/bootcamps/:id
 // @access public
-exports.getBootcamp = async (req, res, next) => {
-  try {
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
-    }
+      return next(
+         new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+     }
     res.status(200).json({ success: true, data: bootcamp });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  }) 
 
 // @desc create new bootcamp
 // @route POST /api/v1/bootcamps/:id
 // @access private
-exports.createBootcamp = async (req, res, next) => {
-  try {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({
       success: true,
       data: bootcamp
     });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
-
+  });
 // @desc update bootcamp
 // @route PUT /api/v1/bootcamps/:id
 // @access public
-exports.updateBootcamp = async (req, res, next) => {
-  try {
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
     });
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+         new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
     res.status(200).json({ success: true, data: bootcamp });
-  } catch (err) {
-    res.status(400).json({ success: false })
-  }
-  
-};
+  });
 
 // @desc delete bootcamp
 // @route DELETE /api/v1/bootcamps/:id
 // @access public
-exports.deleteBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id); 
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+         new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
     res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    res.status(400).json({ success: false })
-  }
-};
+  }); 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
@@ -15,7 +16,7 @@ const bootcamps = require('./routes/bootcamps');
 const app = express();
 
 // Body parser
-app.use(express.json())
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -24,6 +25,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
