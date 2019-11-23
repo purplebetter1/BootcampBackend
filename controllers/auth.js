358b1c6723aa31ef23869211654a24e093382d25
Andrew G <booleanorama@gmail.com>
2019-11-23 12:56:35 -0500

    Add user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc register user
-// @route GET /api/v1/auth/register
+// @route POST /api/v1/auth/register
 // @access public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -17,5 +17,39 @@ exports.register = asyncHandler(async (req, res, next) => {
 		role
 	});
 
-	res.status(200).json({success: true});
+	// Create token
+	const token = user.getSignedJwtToken();
+
+	res.status(200).json({success: true, token});
+	});
+
+// @desc login user
+// @route POST /api/v1/auth/login
+// @access public
+
+exports.login = asyncHandler(async (req, res, next) => {
+	const {  email, password } = req.body
+
+	// Validate email and password
+	if(!email || !password){
+		return next(new ErrorResponse('Please provide an email and password', 400));
+	}
+
+	// Check for user
+	const user = await User.findOne({email}).select('+password');
+
+	if(!user){
+	  return next(new ErrorResponse('Invalid Credentials', 401));	
+	}
+
+	// Check if password matches
+	const isMatch = await user.matchPassword(password);
+	if(!isMatch){
+		return next(new ErrorResponse('Invalid Credentials', 401));
+	}
+
+	// Create token
+	const token = user.getSignedJwtToken();
+
+	res.status(200).json({success: true, token});
 	});
