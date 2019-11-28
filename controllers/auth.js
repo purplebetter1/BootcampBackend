3ef206af72abe1b2aba642259c363aa3da8a1eca
Andrew G <booleanorama@gmail.com>
2019-11-28 13:32:03 -0500

    cookie set

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,9 +18,7 @@ exports.register = asyncHandler(async (req, res, next) => {
 	});
 
 	// Create token
-	const token = user.getSignedJwtToken();
-
-	res.status(200).json({success: true, token});
+	sendTokenResponse(user, 200, res);
 	});
 
 // @desc login user
@@ -49,7 +47,28 @@ exports.login = asyncHandler(async (req, res, next) => {
 	}
 
 	// Create token
-	const token = user.getSignedJwtToken();
+	sendTokenResponse(user, 200, res);
 
-	res.status(200).json({success: true, token});
+	
 	});
+
+	// Get token from model, create cookie, send response
+	const sendTokenResponse = (user, statusCode, res) => {
+		// Create token
+	 const token = user.getSignedJwtToken();
+
+	 const options = {
+	 	expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+	 	httpOnly:true
+	 }
+
+	 if(process.env.NODE_ENV === 'production'){
+	 	options.secure = true;
+	 }
+
+	 res
+	 .status(statusCode)
+	 .cookie('token', token, options)
+	 .json({success: true,
+	 	token})
+	}
