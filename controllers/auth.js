5ce43f9d54615318659464ff4b76ab748e72d046
Andrew G <booleanorama@gmail.com>
2019-12-08 14:43:44 -0500

    Send password change response by email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
+const sendEmail = require('../utils/sendEmail')
 
 // @desc register user
 // @route POST /api/v1/auth/register
@@ -101,7 +102,33 @@ exports.forgotPassword = asyncHandler(async(req, res, next) => {
 
 	await user.save({validateBeforeSave: false});
 
-	console.log(resetToken);
+	//console.log(resetToken);
+
+	// Create reset url
+	const resetUrl = `${req.protocol}://${req.get('host')}api/v1/resetpassword/${resetToken}`;
+
+	const message = `You are receiving this email because you (or someone else) has requested
+	the reset of a password.  Please make a PUT request to: \n\n ${resetUrl}`;
+
+	try {
+		await sendEmail({
+			email: user.email,
+			subject: 'Password reset token',
+			message
+		});
+		res.status(200).json({
+			success: true,
+			data: "Email sent"
+		});
+	}
+	catch (err){
+		console.log(err);
+		user.resetPasswordToken = undefined;
+		user.resetPasswordExpire = undefined;
+
+		await user.save({validateBeforeSave: false});
+		return next(new ErrorResponse('Email could not be sent', 500));
+	}
 
 	res.status(200).json({
 		success: true,
