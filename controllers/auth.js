18b8e243534dcb4ec60b62c568dec1197b18c229
Andrew G <booleanorama@gmail.com>
2019-12-23 14:10:50 -0500

    Prevent NoSQL injection

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,6 +68,21 @@ exports.getMe = asyncHandler(async(req, res, next) => {
 	});
 });
 
+// @desc Log user out/clear cookie
+// @route POST /api/v1/auth/logout
+// @access private
+exports.logout = asyncHandler(async(req, res, next) => {
+	res.cookie('token', 'none', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true
+	});
+
+	res.status(200).json({
+		success: true,
+		data: {}
+	});
+});
+
 // @desc Update user details
 // @route PUT /api/v1/auth/updatedetails
 // @access private
